370952834eb2b3b003241d3bbf088cb39a9ea704
Thorsten Kober <tkober@gmail.com>
2023-05-30 12:19:18 -0700

    fixing

--- components/molecules/Page.vue
+++ components/molecules/Page.vue
@@ -1,8 +1,20 @@
 <script setup lang="js">
-    const route = useRoute()
+    const route = useRoute();
+</script>
+
+<script lang="js">
+    import { content } from "../../data/content";
+    export default {
+        data() {
+            return {
+                content
+            }
+        }
+    }
 </script>
 
 <template>
-    <div>{{ route.name }}</div>
-    <div> Current page: <code>{{ route.path }}</code></div>
+    <div v-for="section in content[route.name].sections">
+        {{ section.name }}
+    </div>
 </template>
