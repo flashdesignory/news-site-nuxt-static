c35e93835317d84399e9efe41aece0d23d2aae58
Thorsten Kober <tkober@gmail.com>
2023-05-31 12:35:26 -0700

    add description

--- app.vue
+++ app.vue
@@ -1,15 +1,24 @@
 <script setup>
-  useHead({
-      title: "The Daily Broadcast",
-      htmlAttrs: {
-        lang: "en"
-      }
-    });
-  </script>
+useHead({
+  title: "The Daily Broadcast",
+  htmlAttrs: {
+    lang: "en"
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'A news site developed with Nuxt.'
+    }
+  ],
+});
+</script>
 
 <template>
   <NuxtLayout>
-    <NuxtPage/>
+    <NuxtPage />
   </NuxtLayout>
   <div id="settings-container"></div>
   <div id="notifications-container"></div>
