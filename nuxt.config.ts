3728748038fbf4f8a42d83dd39b801bff02ba5c6
Thorsten Kober <tkober@gmail.com>
2023-06-02 13:18:43 -0700

    undo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from "path";
 import { defineNuxtConfig } from "nuxt/config";
-import { NuxtPage } from "nuxt/schema";
 
 export default defineNuxtConfig({
     ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
     components: ["~/components", "~/components/assets", "~/components/atoms", "~/components/molecules"],
-    hooks: {
-        "prerender:routes" ({ routes }) {
-            routes.clear();
-            routes.add("/");
-        },
-        "pages:extend"(pages: NuxtPage[]) {
-            for (const name of ["home", "us", "world", "politics", "business", "opinion", "health"]) {
-                pages.push({
-                    name,
-                    path: name === "home" ? "/" : `/${name}`,
-                    file: "~/components/molecules/Page.vue",
-                });
-            }
-        },
-    },
     nitro: {
         output: {
             publicDir: path.join(__dirname, "docs"),
