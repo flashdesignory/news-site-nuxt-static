2be315a4c3c0c6d18db90999fa4d2b151c7f3578
Thorsten Kober <tkober@gmail.com>
2023-06-25 09:12:42 -0700

    use hash router

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,11 @@ export default defineNuxtConfig({
     ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
     components: ["~/components", "~/components/assets", "~/components/atoms", "~/components/molecules"],
+    router: {
+        options: {
+            hashMode: true,
+        },
+    },
     nitro: {
         output: {
             publicDir: path.join(__dirname, "docs"),
