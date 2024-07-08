5128f6eb45a00c18f36c1de6e953ae7458871b4a
Thorsten Kober <tkober@gmail.com>
2024-07-08 11:47:47 -0400

    rebuild

---
+++ docs/_nuxt/health.c8b37bf4.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./Page.c49ff2d5.js";import{a as o,c as t,o as a}from"./entry.2ed651a2.js";const n={};function r(_,s){const c=e;return a(),t(c,{id:"health"})}const i=o(n,[["render",r]]);export{i as default};
+//# sourceMappingURL=health.c8b37bf4.js.map
---
+++ docs/_nuxt/world.dc34a1ba.js
@@ -0,0 +1,2 @@
+import{_ as c}from"./Page.c49ff2d5.js";import{a as r,c as e,o as n}from"./entry.2ed651a2.js";const t={};function _(a,s){const o=c;return n(),e(o,{id:"world"})}const d=r(t,[["render",_]]);export{d as default};
+//# sourceMappingURL=world.dc34a1ba.js.map
